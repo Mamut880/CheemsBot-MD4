9a4d3ace39fbdbfc9d750e2ef42dac94a8a67d4b
Mamut880 <112500060+Mamut880@users.noreply.github.com>
2022-09-30 03:02:14 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -70,7 +70,7 @@ global.botscript = 'https://github.com/DGXeon/CheemsBot-MD4' //script link
 global.reactmoji = "😘" //ur menu react emoji
 global.themeemoji = "🦄" //ur theme emoji
 global.packname = "Rukbat Stickers\n\n\n\n\n\n\n\n\n\n\n🦄🦄🦄 " //ur sticker watermark packname
-global.author = "🦄Dream Guy Xeon" //ur sticker watermark author
+global.author = "🦄RUKBAT" //ur sticker watermark author
 
 //Bot theme media
 global.thum = fs.readFileSync("./XeonMedia/theme/cheemspic.jpg") //ur thumb pic
